6a8b8323073f9596fee30b207a5f61c8934c5622
riv-j <r.jesus9107@gmail.com>
2019-01-19 12:01:36 -0800

    changes

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var arrOfImages = ['imgs/down.jpg', 'imgs/bring.jpg', 'imgs/color.jpg',
                    'imgs/crumb.jpg', 'imgs/currents.jpg', 'imgs/every.jpg', 
                    'imgs/happy.jpg', 'imgs/shadow-work.jpg', 'imgs/wings.jpg'];
 
+window.setTimeout(hideAllCards, 2000)
+
 function doubleImages(arr) {
   for (var i = arr.length - 1; i >= 0; i--) {
     arr.push(arr[i]);
@@ -53,8 +55,33 @@ function hideACard(idx){
   specifiedCard.src = 'imgs/questionMark.png'
 }
 
-for(var i = 0; i < arrOfImages.length; i++){
-  hideACard(i)
+function hideAllCards(){
+  for (var i = 0; i < arrOfImages.length; i++) {
+    hideACard(i)
+  }
+}
+
+var attempts = 0
+function wrong() {
+  attempts++
+  if (attempts >= 5) {
+    alert('GAME OVER!')
+  }
+}
+
+// generates message 
+function message(condition){
+  var node = document.getElementById('p');
+  var text;
+  if(condition == 'match'){
+    text = document.createTextNode('They match!');
+    node.appendChild(text);
+  } else if(condition == 'no match'){
+    text = document.createTextNode('Try again!');
+    node.appendChild(text);
+  } 
+  var remove = () => node.removeChild(text);
+  window.setTimeout(remove, 1000)
 }
 
 var cardsPicked = []
@@ -72,11 +99,14 @@ function revealCard(event) {    // this time, the click event is going to be the
   if(cardsPicked.length == 2){
     if(arrOfImages[cardsPicked[0]] == arrOfImages[cardsPicked[1]]){
       cardsPicked = []
+      message('match')
     } else {
       var hidePickedCards = function(){
         hideACard(cardsPicked[0]);
         hideACard(cardsPicked[1]);
         cardsPicked = []
+        message('no match')
+        wrong()
       }
       window.setTimeout(hidePickedCards, 1000);
     }
@@ -87,3 +117,20 @@ var cards = document.getElementsByClassName("card");    // grab all the cards
 for (var i = 0; i < cards.length; i++) {
   cards[i].addEventListener("click", revealCard);
 }
+
+function gameOver(){
+  var newP = document.createElement('p');
+  newP.id = 'alert'
+  var over = document.getElementById('alert')
+  var text = 'GAME OVER'
+  over.appendChild(text)
+}
+
+// for(var i = 0; i < arrOfImages.length; i++){
+//   var node = document.getElementById('p');
+//   var text;
+//   if (arrOfImages[i] !== 'imgs/questionMark.png'){
+//     text = document.createTextNode('All Match!');
+//     node.appendChild(text);
+//   } 
+// }
